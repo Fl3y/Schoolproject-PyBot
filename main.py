83094b9fb47bba3cd44d168a7f41eb1f15de8678
Fl3y <65833494+Fl3y@users.noreply.github.com>
2022-04-24 21:59:16 +0200

    Bot Setup and can start; Response does not work rn

---
+++ main.py
@@ -0,0 +1,35 @@
+
+import discord 
+from decouple import config
+
+TOKEN = config("TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_Message(message):
+    username = str(message.author).split("#")[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f"{username}: {user_message} ({channel})")
+
+    if message.author == client.user:
+        return
+    
+    if message.channel.name == "general-bot":
+        if user_message.lower() == "hello":
+            await message.channel.send(f"Hello {username}!")
+            return
+
+        elif user_message.lower() == "bye":
+            await message.channel.send(f"See you later {username}!")
+            return
+        
+
+client.run(TOKEN)
+
+
