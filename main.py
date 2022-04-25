f2f61c891cbb36764a376d702a6f590c234d884f
Fl3y <65833494+Fl3y@users.noreply.github.com>
2022-04-25 12:27:39 +0200

    Bot can now access events and is able to run command

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import dotenv
 import discord
 from discord.ext import commands
 
-intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
+
 dotenv.load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
+
 client = commands.Bot(command_prefix='.', intents=intents)
-
-
 
 
 @client.event
@@ -18,33 +18,19 @@ async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
 @client.event 
-async def on_Member_join(member):
+async def on_member_join(member):
     print(f"{member} has joint the server,")
+    return
 
 @client.event 
-async def on_Member_remove(member):
+async def on_member_remove(member):
     print(f"{member} has left the server,")
+    return
 
-@client.event
-async def on_Message(message):
-    username = str(message.author).split("#")[0]
-    user_message = str(message.content)
-    channel = str(message.channel.name)
-    print(f"{username}: {user_message} ({channel})")
-
-    if message.author == client.user:
-        return
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)} ms")
     
-    if message.channel.name == "general-bot":
-        if user_message.lower() == "hello":
-            await message.channel.send(f"Hello {username}!")
-            return
-
-        elif user_message.lower() == "bye":
-            await message.channel.send(f"See you later {username}!")
-            return
-        
-
 client.run(TOKEN)
 
 
