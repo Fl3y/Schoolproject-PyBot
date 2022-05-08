0ffe605938abcaab097f4c7e012ca005da9b6970
Fl3y <65833494+Fl3y@users.noreply.github.com>
2022-05-08 03:12:07 +0200

    newFirebase

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ import json
 import re
 import music
 import ReputationScore
-import asyncio
-import aiomysql
-import time
-from mysql.connector import MySQLConnection, Error
-from python_mysql_dbconfig import read_db_config
+
+
 
 
 
@@ -37,31 +34,8 @@ client = commands.Bot(command_prefix='.', intents=intents)
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-def connect():
-    """ Connect to MySQL database """
-
-    db_config = read_db_config()
-    conn = None
-    try:
-        print('Connecting to MySQL database...')
-        conn = MySQLConnection(**db_config)
-
-        if conn.is_connected():
-            print('Connection established.')
-        else:
-            print('Connection failed.')
-
-    except Error as error:
-        print(error)
-
-    finally:
-        if conn is not None and conn.is_connected():
-            conn.close()
-            print('Connection closed.')
 
 
-if __name__ == '__main__':
-    connect()
 
 @client.event
 async def on_ready():
